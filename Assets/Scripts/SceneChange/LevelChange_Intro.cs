03a15fa5094966f8860c3a6ffcc7ef6de41c1679
Duddypai <arnaupardo95@gmail.com>
2024-06-10 19:13:30 +0200

    Omitir Introducción
    Ahora se puede omitir la introducción pulsando la tecla escape.
    Se ha añadido una imagen informativa sobre esto al jugador, que aparece al empezar la introducción y cada vez que el jugador pulsa una tecla que no es escape.

--- Assets/Scripts/SceneChange/LevelChange_Intro.cs
+++ Assets/Scripts/SceneChange/LevelChange_Intro.cs
@@ -2,25 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEditor.VersionControl;
 
 public class LevelChange_Intro : MonoBehaviour
 {
 
     public float changeTime;
     public string sceneName;
-    AudioManager audioManager;
+    private AudioManager audioManager;
+
+    //Intro Message
+    [SerializeField] private Animator anim;
+    private float timer = 4f;
+    private float timeCount;
 
     public void Awake() {
-        audioManager = GameObject.FindGameObjectWithTag(GameTags.audio).GetComponent<AudioManager>();
+        audioManager = GameObject.FindGameObjectWithTag(GameTags.audio).GetComponent<AudioManager>(); 
+
+        StartCoroutine(SkipIntro());
+
+        timeCount = timer;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Cuando el jugador pulsa cualquier tecla, le sale el aviso de omitir. Ponemos un contador para que las animación funcionen bien, sino se repite el FadeOut 2 veces reiniciando la imagen cuando no debe
+        if (Input.anyKeyDown && timeCount <= 0)
+        {
+            StartCoroutine(SkipIntro());
+            timeCount = timer;
+        }
+
+        if (timeCount > 0)
+        {
+            timeCount -= Time.deltaTime;
+        }
+
+        //Pulsa ESC, cambia de escena
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(sceneName);
+            audioManager.Nivel1();
+        }
+
+        //Al acabar el tiempo, cambia de escena
         changeTime -= Time.deltaTime;
         if(changeTime <= 0) {
             SceneManager.LoadScene(sceneName);
             audioManager.Nivel1();
         }
+    }
+
+    private IEnumerator SkipIntro()
+    {
+        yield return new WaitForSeconds(1f);
+        //FadeIn
+        anim.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(3f);
+        //FadeOut
+        anim.SetTrigger("FadeOut");
     }
 }
