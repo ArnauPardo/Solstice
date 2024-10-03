39720b6308af122be98e82bb7b5a8e0419e24b48
Arnau Pardo <arnaupardo95@gmail.com>
2024-10-03 20:37:13 +0200

    Escena_05 modificada y ajuste animación salto
    1. Con el anterior commit, había cambiado el código del salto largo/corto y provocaba que con el simple movimiento lateral del jugador, se lanzara la animación de salto. He vuelto al código anterior.
    2. Ajustado el tamaño en Y del boxSize del ground detector del player para evitar que haciendo movimientos rápidos entre derecha e izquierda (a-d-a-d-a-d) no detectara bien el suelo y utilizaba la animación de salto.
    3. He modificado la parte izquierda de la escena 05, había un par de zonas que parecía que se podía llegar con un simple salto y se lo he corregido para que se vea claramente que no se puede llegar

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -127,19 +127,16 @@ public class PlayerControl : MonoBehaviour
             jumpTimer = 0.02f;
         }
 
-        // Mejora de salto, salto largo y corto solo si estamos en el aire
-        if (!isGrounded)
+        //Mejora de salto, salto largo y corto.
+        //El if es para que cuando se detecte que el jugador esta cayendo y aplique la fuerza de la gravedad normal, por eso el valor de saltoCorto es 1
+        if (rb.velocity.y < 0)
         {
-            // Si el jugador está cayendo, aplica la gravedad aumentada para un salto largo
-            if (rb.velocity.y < 0)
-            {
-                rb.velocity += Vector2.up * Physics2D.gravity.y * longJump * Time.deltaTime;
-            }
-            // Si está subiendo pero no mantiene el botón de salto, aplica la gravedad para un salto corto
-            else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-            {
-                rb.velocity += Vector2.up * Physics2D.gravity.y * shortJump * Time.deltaTime;
-            }
+            rb.velocity += Vector2.up * Physics2D.gravity.y * longJump * Time.deltaTime;
+        }
+        //Si estamos saltando pero no estamos pulsando el boton, aplicamos una fuerza mayor en Y para que el jugador deje de subir antes y entre en ca�da
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * shortJump * Time.deltaTime;
         }
     }
 
