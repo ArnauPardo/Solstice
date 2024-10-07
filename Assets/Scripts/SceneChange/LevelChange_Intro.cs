bbccabe94d6e3a58f8cfe201e0f841685563bbc3
Arnau Pardo <arnaupardo95@gmail.com>
2024-10-07 19:47:28 +0200

    Ajustes del proyecto

--- Assets/Scripts/SceneChange/LevelChange_Intro.cs
+++ Assets/Scripts/SceneChange/LevelChange_Intro.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-using UnityEditor.VersionControl;
 
 public class LevelChange_Intro : MonoBehaviour
 {
